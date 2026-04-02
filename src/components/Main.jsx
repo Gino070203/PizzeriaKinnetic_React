865cee9fc9b75213b482a6016418351ddded5780
Gino070203 <ginoemch132@gmail.com>
2026-04-02 02:59:36 -0500

    primer commit del componente main

---
+++ src/components/Main.jsx
@@ -0,0 +1,54 @@
+
+import styleHero from "../styles/sectionHero.module.css"
+
+
+function SectionHero(){
+
+
+    return(
+        <section className={styleHero.hero}>
+
+            <div className={`${styleHero.containerInformation}`}>
+
+                <div className={styleHero.presentation}>
+
+                    <h1>¡La mejor pizzeria de Villa Salvador!</h1>
+                    <h3>Ven y disfruta una momento unico</h3>
+
+                </div>
+
+
+                <div className={styleHero.containerButtons}>
+
+                    <a href="">Ver menu</a>
+                    
+                    <a href="">Reservar</a>
+
+                </div>
+
+
+            </div>
+
+
+        </section>
+    )
+
+}
+
+
+export function MainPizza(){
+
+
+    return(
+
+        <main>
+
+            <SectionHero />
+
+
+
+        </main>
+
+    )
+
+}
