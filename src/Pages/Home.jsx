8a9a9838d968782c180ccb6b4c598a54c0b5a2e0
Gino070203 <ginoemch132@gmail.com>
2026-04-04 01:56:18 -0500

    Este component contiene el header,main y footer

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,32 @@
+
+import {HeaderPizzeria} from "../components/Header/Header.jsx"
+import {SectionHero} from "../sections/Hero/Hero.jsx"
+import {SectionFeatures} from "../sections/Features/Features.jsx"
+import { SectionProducts } from "../sections/Products/Products.jsx"
+import { VideoSection } from "../sections/VideoSection/VideoSection.jsx"
+import { Contact } from "../sections/Contact/Contact.jsx"
+
+export function HomePage(){
+
+    return(
+
+        <>
+        
+        <HeaderPizzeria />
+
+        <main>
+
+            <SectionHero />
+            <SectionFeatures />
+            <SectionProducts />
+            <VideoSection />
+            <Contact />
+
+        </main>
+
+        </>
+
+    )
+
+
+}
